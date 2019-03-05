017e4cf26c8362781e2128dde3d512054dbb6398
Laura <Lhuiefiction@gmail.com>
2019-03-04 19:21:46 -0500

    Study study

---
+++ StarWarsAPI/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using StarWarsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StarWarsAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("GetStarWars");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult GetStarWars()
+        {
+
+            HttpWebRequest WR = WebRequest.CreateHttp("https://swapi.co/api/people/");
+
+            HttpWebResponse Response = (HttpWebResponse)WR.GetResponse();
+
+            StreamReader data = new StreamReader(Response.GetResponseStream());
+
+            string JsonData = data.ReadToEnd();
+
+            JObject SWData = JObject.Parse(JsonData);
+
+            List<StarWars> people = new List<StarWars>();
+            for (int i = 0; i < SWData.Count; i++)
+            {
+                string name = SWData["results"][i]["name"].ToString();
+                string birthyear = SWData["results"][i]["birth_year"].ToString();
+                string films = SWData["results"][i]["films"].ToString(); //this won't work
+                string vehicles = SWData["results"][i]["vehicles"].ToString();//this also won't work
+                StarWars person = new StarWars(name, birthyear, films, vehicles);
+                people.Add(person);
+
+            }
+            ViewBag.People = people;
+            return View();
+        }
+    }
+}
---
+++ StarWarsAPI/Models/StarWars.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StarWarsAPI.Models
+{
+    public class StarWars
+    {
+        
+        public string Name;
+        public string BirthYear;
+        public string Films;
+        public string Vehicles;
+
+        public StarWars(string name, string birthyear, string films, string vehicles)
+        {
+            Name = name;
+            BirthYear = birthyear;
+            Films = films;
+            Vehicles = vehicles;
+        }
+
+        public StarWars() { }
+
+    }
+}
